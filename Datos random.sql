7c2b9fd35fef2d8407faa056b4c1112a22f5109e
Lestat9812 <fersivelli98@gmail.com>
2024-02-26 15:42:35 -0600

    Datos y esquema para poblar base

---
+++ Datos random.sql
@@ -0,0 +1,138 @@
+INSERT INTO `estudiantes` VALUES
+(8,'2023-05-08 07:25:04.000','1988-03-17 05:56:40.000',NULL,'Tremayne','Wisoky','Weissnat','1','1','38 ob','cn','1996-12-11 06:09:13.000',9,'','39 jm','738','98876 Gennaro Pass Apt. 302','08541','South','haven','Cartwrightmouth','District of Columbia',92534,'Saulbury','Virginia','breitenberg.whitney@yahoo.com',NULL,'','','','','0000-00-00 00:00:00.000','','',NULL,0,'',NULL,NULL,'',NULL,NULL,NULL,'brath@example.org',NULL,'',0,'0000-00-00 00:00:00.000','','',0,0,0,'',0,0,0,0,0,0,0,0,0,0,NULL,NULL,NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL,0),
+(6,'2014-08-07 20:38:45.000','2000-04-21 22:41:35.000',NULL,'Kasey','Bauch','Wilkinson','','','48 fb','cn','2014-09-16 13:58:01.000',9,'','70 cv','585','105 Gislason Corner','2223','Lake','port','East Keeganport','California',79661,'Ceciliaside','Wyoming','joel.lind@gmail.com',NULL,'','','','','0000-00-00 00:00:00.000','','',NULL,0,'',NULL,NULL,'',NULL,NULL,NULL,'jayson.conn@example.net',NULL,'',0,'0000-00-00 00:00:00.000','','',0,0,0,'',0,0,0,0,0,0,0,0,0,0,NULL,NULL,NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL,0),
+(2,'2010-01-08 23:44:47.000','2006-06-05 12:46:56.000',NULL,'Katrine','Tremblay','Gorczany','1','','14 el','en','2018-01-15 05:09:17.000',8,'','82 no','565','31139 Eichmann Way','648','West','borough','Gutkowskiland','Connecticut',42578,'Friesenview','Arkansas','hoeger.kory@gmail.com',NULL,'','','','','0000-00-00 00:00:00.000','','',NULL,0,'',NULL,NULL,'',NULL,NULL,NULL,'bobbie.metz@example.org',NULL,'',0,'0000-00-00 00:00:00.000','','',0,0,0,'',0,0,0,0,0,0,0,0,0,0,NULL,NULL,NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL,0),
+(4,'2004-07-02 15:32:58.000','1985-09-15 21:57:59.000',NULL,'Lenore','Ritchie','Lynch','','','24 gv','cn','2015-02-08 22:37:31.000',8,'','09 bz','224','14369 Trantow Turnpike Suite 983','87934','Port','fort','Willowton','Virginia',7787,'Gislasonfort','Michigan','everardo38@yahoo.com',NULL,'','','','','0000-00-00 00:00:00.000','','',NULL,0,'',NULL,NULL,'',NULL,NULL,NULL,'prosacco.jamey@example.com',NULL,'',0,'0000-00-00 00:00:00.000','','',0,0,0,'',0,0,0,0,0,0,0,0,0,0,NULL,NULL,NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL,0),
+(7,'1981-02-23 00:37:03.000','2015-11-23 00:40:29.000',NULL,'Yazmin','Collins','Lemke','1','','38 tq','pt','1984-11-07 15:32:03.000',3,'','84 cg','032','3009 Tromp Station','49867','North','mouth','North Elza','NorthDakota',61423,'Lake Roberto','Tennessee','danielle14@yahoo.com',NULL,'','','','','0000-00-00 00:00:00.000','','',NULL,0,'',NULL,NULL,'',NULL,NULL,NULL,'rosalee.mcclure@example.com',NULL,'',0,'0000-00-00 00:00:00.000','','',0,0,0,'',0,0,0,0,0,0,0,0,0,0,NULL,NULL,NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL,0),
+(5,'1974-04-06 16:56:19.000','1977-12-12 01:48:39.000',NULL,'Bret','Ankunding','Jones','','1','85 bg','es','1973-07-30 09:56:52.000',1,'','07 bo','070','492 O\Keefe Pines Suite 596','4404','New','town','Lake Sydneeview','Washington',71269,'Port Dolores','Kansas','tmarquardt@yahoo.com',NULL,'','','','','0000-00-00 00:00:00.000','','',NULL,0,'',NULL,NULL,'',NULL,NULL,NULL,'kenton.flatley@example.net',NULL,'',0,'0000-00-00 00:00:00.000','','',0,0,0,'',0,0,0,0,0,0,0,0,0,0,NULL,NULL,NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL,0),
+(3,'1971-09-03 00:13:09.000','2008-07-17 05:10:45.000',NULL,'Anya','Friesen','Schneider','','1','44 ss','it','1992-12-15 14:03:22.000',4,'','53 oj','756','0479 Spinka Road','17629','Port','stad','Beahantown','WestVirginia',85493,'Hamillchester','Missouri','lrempel@yahoo.com',NULL,'','','','','0000-00-00 00:00:00.000','','',NULL,0,'',NULL,NULL,'',NULL,NULL,NULL,'vmann@example.org',NULL,'',0,'0000-00-00 00:00:00.000','','',0,0,0,'',0,0,0,0,0,0,0,0,0,0,NULL,NULL,NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL,0),
+(9,'1975-01-03 14:29:34.000','1987-11-09 07:01:43.000',NULL,'Krystel','Wisoky','Reichert','1','1','34 it','pt','1978-07-03 12:28:47.000',9,'','23 fu','571','8705 Murazik Branch','57763','West','berg','Lake Rosie','NewJersey',24551,'West Stephanfort','NewJersey','lesch.lacey@hotmail.com',NULL,'','','','','0000-00-00 00:00:00.000','','',NULL,0,'',NULL,NULL,'',NULL,NULL,NULL,'akunze@example.net',NULL,'',0,'0000-00-00 00:00:00.000','','',0,0,0,'',0,0,0,0,0,0,0,0,0,0,NULL,NULL,NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL,0),
+(10,'1971-11-04 22:24:32.000','1980-04-15 17:47:35.000',NULL,'Marlin','Kreiger','Fadel','','1','22 ca','ru','1982-12-02 17:24:01.000',5,'','16 br','586','655 Dietrich Lights Suite 018','62116','South','burgh','East Ezraside','NewHampshire',5019,'Grahamchester','Tennessee','america.reichert@yahoo.com',NULL,'','','','','0000-00-00 00:00:00.000','','',NULL,0,'',NULL,NULL,'',NULL,NULL,NULL,'rubie76@example.com',NULL,'',0,'0000-00-00 00:00:00.000','','',0,0,0,'',0,0,0,0,0,0,0,0,0,0,NULL,NULL,NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL,0),
+(1,'1971-04-08 02:52:30.000','2012-08-22 02:09:24.000',NULL,'Andreane','Walter','Wiza','','1','14 jv','cn','2016-05-09 00:12:24.000',6,'','01 yd','913','7299 Ethyl Radial Suite 732','49660','East','borough','Josianebury','Missouri',67440,'Koelpinborough','Michigan','herminio68@yahoo.com',NULL,'','','','','0000-00-00 00:00:00.000','','',NULL,0,'',NULL,NULL,'',NULL,NULL,NULL,'daniella61@example.org',NULL,'',0,'0000-00-00 00:00:00.000','','',0,0,0,'',0,0,0,0,0,0,0,0,0,0,NULL,NULL,NULL,NULL,0,NULL,NULL,NULL,NULL,NULL,NULL,0);
+
+INSERT INTO `ut_campus` VALUES
+(1,NULL,NULL,NULL,'Hessel Inc','MI','9882 Vance Cape Suite 145\nReichelview, ND 05147'),
+(2,NULL,NULL,NULL,'Harber Group','VA','992 Demond Crossing Suite 685\nFannyburgh, MD 88625'),
+(3,NULL,NULL,NULL,'Mohr, Rohan and Wisoky','ID','233 O\Hara Pine Apt. 095\nNorth Maymieton, NJ 78426-1511');
+
+INSERT INTO `perfiles` VALUES
+(1,NULL,NULL,NULL,'illo'),
+(5,NULL,NULL,NULL,'ea'),
+(3,NULL,NULL,NULL,'suscipit'),
+(2,NULL,NULL,NULL,'in'),
+(4,NULL,NULL,NULL,'minima');
+
+INSERT INTO `periodos` VALUES
+(1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(2,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
+INSERT INTO `personal` VALUES
+(2,NULL,NULL,NULL,NULL,'Eveline','Wisozk','Frami','albin35','f8c429c5de6587848361db4f5997501ab57e9698ffe563a64ef124834d32e561',NULL,NULL),
+(9,NULL,NULL,NULL,NULL,'Aaliyah','Bahringer','Cummings','hpaucek','5ad85525407a5079b08d026f6654e39b0a61198ef72fed0b3bde7828de68fbd1',NULL,NULL),
+(7,NULL,NULL,NULL,NULL,'Moses','Schulist','Kling','cathrine28','b6db6d0eb9ae51684803456866d3f6b94b944bdea98ea7c32ccf6ea317b1f641',NULL,NULL),
+(10,NULL,NULL,NULL,NULL,'Savion','Becker','Moen','mallie21','e7ee3ea3f6ceb99fa76bd01a245dcbd841e0b9e4586aadb22c0502a3898469f8',NULL,NULL),
+(6,NULL,NULL,NULL,NULL,'Peggie','Von','Cummings','kendall98','9ef4a527dd6ae2f3ab5357f28fbfcf485808605a3a91eb99f0cbcd1e7f8381be',NULL,NULL),
+(5,NULL,NULL,NULL,NULL,'Moshe','Little','Schowalter','satterfield.dejah','bb6d51e879a664b297be06061014875e4cd94624430e7dc171f96f240d7f3310',NULL,NULL),
+(8,NULL,NULL,NULL,NULL,'Katheryn','Rempel','Fisher','olson.brody','ed7f92778146de806a32df6df5b0f1593616d1aada2bea30a26cb67838ebff49',NULL,NULL),
+(3,NULL,NULL,NULL,NULL,'Karson','Moen','Jones','hartmann.melody','9885590164d06642bd351961b555e9e1e918bf1a84da5b51d7443b525f93d6ad',NULL,NULL),
+(1,NULL,NULL,NULL,NULL,'Juanita','Runolfsdottir','Moore','karl.graham','cd342c2ba04813e7679b515c836553f5d19eea97187fe5d5b1e6c159bc6f8988',NULL,NULL),
+(4,NULL,NULL,NULL,NULL,'Tad','Cormier','Crist','jacobs.laurel','683cefbd76fa6038f0ad10a25c7314c9a6fe9ec77c75bbcd952032a17d5269ed',NULL,NULL);
+
+INSERT INTO `carreras` VALUES
+(8,NULL,NULL,NULL,'Alice; \'you needn\'t be so kind,\' Alice replied, rather shyly, \'I--I hardly know, sir, just at first, perhaps,\' said the Dodo said, \'EVERYBODY has won, and all the children she knew, who might do.','',0,NULL,0,0,NULL,0,0,0,1,1),
+(6,NULL,NULL,NULL,'As for pulling me out of sight: \'but it doesn\'t matter a bit,\' said the Mock Turtle. \'Hold your tongue!\' added the Dormouse. \'Don\'t talk nonsense,\' said Alice more boldly: \'you know you\'re growing.','',0,NULL,0,0,NULL,0,0,0,2,2),
+(10,NULL,NULL,NULL,'Mouse. \'Of course,\' the Dodo could not join the dance? Will you, won\'t you, will you, won\'t you join the dance? Will you, won\'t you, will you join the dance. Would not, could not, would not, could.','',0,NULL,0,0,NULL,0,0,0,3,3),
+(5,NULL,NULL,NULL,'I give it up,\' Alice replied: \'what\'s the answer?\' \'I haven\'t opened it yet,\' said Alice; \'but a grin without a porpoise.\' \'Wouldn\'t it really?\' said Alice a little irritated at the door-- Pray,.','',0,NULL,0,0,NULL,0,0,0,1,4),
+(3,NULL,NULL,NULL,'Alice. \'Then it doesn\'t matter which way I want to go! Let me see: I\'ll give them a railway station.) However, she got into it), and sometimes she scolded herself so severely as to size,\' Alice.','',0,NULL,0,0,NULL,0,0,0,2,5),
+(7,NULL,NULL,NULL,'Though they were all turning into little cakes as they would die. \'The trial cannot proceed,\' said the King; \'and don\'t look at me like that!\' He got behind him, and said to herself, \'if one only.','',0,NULL,0,0,NULL,0,0,0,3,6),
+(2,NULL,NULL,NULL,'I am so VERY tired of being upset, and their slates and pencils had been of late much accustomed to usurpation and conquest. Edwin and Morcar, the earls of Mercia and Northumbria--\"\' \'Ugh!\' said the.','',0,NULL,0,0,NULL,0,0,0,1,7),
+(4,NULL,NULL,NULL,'Duchess said after a few yards off. The Cat seemed to be Number One,\' said Alice. \'You are,\' said the Caterpillar. \'Is that the meeting adjourn, for the White Rabbit: it was a most extraordinary.','',0,NULL,0,0,NULL,0,0,0,2,8),
+(9,NULL,NULL,NULL,'Alice did not look at the top of his teacup and bread-and-butter, and then at the Duchess sang the second thing is to give the hedgehog a blow with its head, it WOULD twist itself round and round.','',0,NULL,0,0,NULL,0,0,0,3,9),
+(1,NULL,NULL,NULL,'Alice; \'living at the Caterpillar\s making such a thing. After a time there were ten of them, and considered a little, and then quietly marched off after the candle is blown out, for she was.','',0,NULL,0,0,NULL,0,0,0,1,10);
+
+INSERT INTO `alumnos` VALUES
+(2,NULL,NULL,NULL,'oz871324','','40da9a021dadf95a0de12d0959ba619fe2a2a99df7f916ae030716f2538ed6e2',NULL,0,0,0,0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,1,10),
+(4,NULL,NULL,NULL,'hp036256','','cb5295eb5e9bd1738d7bd8d0d5d35d063f21104f6b0cde6589196072af1f88e7',NULL,0,0,0,0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,2,1),
+(6,NULL,NULL,NULL,'pm292287','','609cdad9496fdc821f8abf88dee10b42fdf56af3dc7f4a37a0b137b523affbc4',NULL,0,0,0,0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,3,2),
+(3,NULL,NULL,NULL,'us885723','','6cfdb9332a372e0aad4143e4bd9e7d6bf245c9dcf2d14132be9b924407aaa98f',NULL,0,0,0,0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,4,3),
+(5,NULL,NULL,NULL,'nv249348','','e2497df92bbafde10822cfbfb30648a610a0e8d37c4256a2d1784059836220d8',NULL,0,0,0,0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,5,4),
+(1,NULL,NULL,NULL,'ca785125','','fed7273aa9ad08d0eab892fd9489420620211d6e409dbeb7b85a0d321667a526',NULL,0,0,0,0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,6,5),
+(9,NULL,NULL,NULL,'fb344487','','a2df2f2aa29f7a0dacf40177c90e2aeb8ef2b624f94f6ffed10c469f0e91d57b',NULL,0,0,0,0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,7,6),
+(7,NULL,NULL,NULL,'su387342','','459f671814a39d5095627531a9c188987aef13d3355ce92c470da09219e478a1',NULL,0,0,0,0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,8,7),
+(8,NULL,NULL,NULL,'ln592852','','1c96dcf12002d714f9bf41df4d53dc7d30ce5d38d5546149b1cda108b773b303',NULL,0,0,0,0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,9,8),
+(10,NULL,NULL,NULL,'ud505667','','b10c57f6c7a54e5d73891bf6f111f04f9d421eddf1c20074b20184a24efaa1a8',NULL,0,0,0,0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000',0,'0000-00-00 00:00:00.000','0000-00-00 00:00:00.000',0,10,9);
+
+INSERT INTO `grupos` VALUES
+(6,NULL,NULL,NULL,'eos',0,1,1,0,1),
+(1,NULL,NULL,NULL,'maxime',0,2,2,0,2),
+(2,NULL,NULL,NULL,'nostrum',0,3,1,0,3),
+(9,NULL,NULL,NULL,'laborum',0,4,2,0,4),
+(8,NULL,NULL,NULL,'et',0,5,1,0,5),
+(7,NULL,NULL,NULL,'enim',0,6,2,0,6),
+(4,NULL,NULL,NULL,'aliquid',0,7,1,0,7),
+(10,NULL,NULL,NULL,'aut',0,8,2,0,8),
+(3,NULL,NULL,NULL,'animi',0,9,1,0,9),
+(5,NULL,NULL,NULL,'placeat',0,10,2,0,10);
+
+INSERT INTO `materias` VALUES
+(3,NULL,NULL,NULL,'Praesentium aut quia est odit minima perferendis ipsa.',NULL,0,1,0),
+(4,NULL,NULL,NULL,'Est harum et quis dicta necessitatibus corporis vel.',NULL,0,2,1),
+(6,NULL,NULL,NULL,'Atque nulla sapiente et.',NULL,0,3,0),
+(5,NULL,NULL,NULL,'A quisquam expedita totam dolore qui.',NULL,0,4,0),
+(9,NULL,NULL,NULL,'Asperiores molestiae aliquam nulla est optio debitis illo voluptate.',NULL,0,5,1),
+(2,NULL,NULL,NULL,'Vitae voluptatem vitae consequatur et illum repellat.',NULL,0,6,1),
+(7,NULL,NULL,NULL,'Dolores esse qui eos atque.',NULL,0,7,1),
+(10,NULL,NULL,NULL,'Vero velit iure voluptas consequatur.',NULL,0,8,0),
+(1,NULL,NULL,NULL,'Veniam quae fugit omnis alias.',NULL,0,9,1),
+(8,NULL,NULL,NULL,'Ratione blanditiis dolores doloribus maiores est ducimus.',NULL,0,10,1);
+
+INSERT INTO `alumno_grupos` VALUES
+(6,NULL,NULL,NULL,7,10,NULL,NULL),
+(8,NULL,NULL,NULL,6,1,NULL,NULL),
+(10,NULL,NULL,NULL,3,9,NULL,NULL),
+(5,NULL,NULL,NULL,8,4,NULL,NULL),
+(2,NULL,NULL,NULL,7,1,NULL,NULL),
+(9,NULL,NULL,NULL,5,4,NULL,NULL),
+(3,NULL,NULL,NULL,6,9,NULL,NULL),
+(1,NULL,NULL,NULL,1,6,NULL,NULL),
+(4,NULL,NULL,NULL,9,2,NULL,NULL),
+(7,NULL,NULL,NULL,7,7,NULL,NULL);
+
+INSERT INTO `grupos_materias` VALUES
+(8,NULL,NULL,NULL,5,3,1,NULL,NULL,NULL,NULL,NULL,NULL),
+(9,NULL,NULL,NULL,4,6,2,NULL,NULL,NULL,NULL,NULL,NULL),
+(5,NULL,NULL,NULL,2,5,3,NULL,NULL,NULL,NULL,NULL,NULL),
+(6,NULL,NULL,NULL,10,6,4,NULL,NULL,NULL,NULL,NULL,NULL),
+(1,NULL,NULL,NULL,10,4,5,NULL,NULL,NULL,NULL,NULL,NULL),
+(2,NULL,NULL,NULL,9,7,6,NULL,NULL,NULL,NULL,NULL,NULL),
+(7,NULL,NULL,NULL,10,10,7,NULL,NULL,NULL,NULL,NULL,NULL),
+(4,NULL,NULL,NULL,2,8,8,NULL,NULL,NULL,NULL,NULL,NULL),
+(10,NULL,NULL,NULL,9,9,9,NULL,NULL,NULL,NULL,NULL,NULL),
+(3,NULL,NULL,NULL,8,2,10,NULL,NULL,NULL,NULL,NULL,NULL);
+
+INSERT INTO `calificaciones` VALUES
+(4,NULL,NULL,NULL,9,1,NULL,NULL,NULL),
+(9,NULL,NULL,NULL,10,10,NULL,NULL,NULL),
+(7,NULL,NULL,NULL,7,2,NULL,NULL,NULL),
+(10,NULL,NULL,NULL,7,7,NULL,NULL,NULL),
+(6,NULL,NULL,NULL,5,1,NULL,NULL,NULL),
+(5,NULL,NULL,NULL,4,7,NULL,NULL,NULL),
+(2,NULL,NULL,NULL,2,6,NULL,NULL,NULL),
+(8,NULL,NULL,NULL,7,5,NULL,NULL,NULL),
+(3,NULL,NULL,NULL,8,4,NULL,NULL,NULL),
+(1,NULL,NULL,NULL,8,9,NULL,NULL,NULL);
+
+INSERT INTO `usuarios_perfiles` VALUES
+(3,NULL,NULL,NULL,10,2),
+(1,NULL,NULL,NULL,5,5),
+(2,NULL,NULL,NULL,1,3),
+(8,NULL,NULL,NULL,3,1),
+(9,NULL,NULL,NULL,10,2),
+(4,NULL,NULL,NULL,9,1),
+(6,NULL,NULL,NULL,1,3),
+(10,NULL,NULL,NULL,3,1),
+(5,NULL,NULL,NULL,3,3),
+(7,NULL,NULL,NULL,4,2);
+
+
+
